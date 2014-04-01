efe3e9918962f3ce802a4a4f4334e2dead32257c
Maxime Vidori <maxime.vidori@enovance.com>
2014-04-01 18:40:43 +0200

    Add template loading from html
    Change-Id: I4eb68d1cfbbb12c0fb765bc1d941f2d72dfb55c8

--- horizon/static/horizon/js/angular/controllers/instances.js
+++ horizon/static/horizon/js/angular/controllers/instances.js
@@ -8,24 +8,7 @@
           var modalInstance = $modal.open({
             windowClass: ['fullscreen'],
             keyboard: false,
-            template: '<div class="modal-header">' +
-              '<h3>I\'m a modal!</h3>' +
-              '</div>' +
-              '<div class="modal-body">' +
-              '<tabset class="wizard">' +
-              '<tab>' +
-              '<tab-heading>' +
-              '<i class="glyphicon glyphicon-bell"></i> Alert!' +
-              '</tab-heading>' +
-              '<div style="height: 200px">' +
-              '</div></tab>' +
-              '<tab heading="Vertical 2">Vertical content 2</tab>' +
-              '</tabset>' +
-              '</div>' +
-              '<div class="modal-footer">' +
-              '<button class="btn btn-primary" ng-click="ok()">OK</button>' +
-              '<button class="btn btn-warning" ng-click="cancel()">Cancel</button>' +
-              '</div>',
+            templateUrl: 'InstanceModal.html',
             controller: function ($scope, $modalInstance) {
               $scope.ok = function () {
                 $modalInstance.close('ok');
