9847ef834ec8267b57b184bc03694d92f817d5a9
Maxime Vidori <maxime.vidori@gmail.com>
2014-07-29 14:54:39 +0200

    FIX IAAS-1881 Better message for network selection
    	Add understandable help message for network list and security group list
    	if no items are selected, show the help message for user,
    	to get more understanding.

--- horizon/static/horizon/js/angular/directives/launch.instance.js
+++ horizon/static/horizon/js/angular/directives/launch.instance.js
@@ -4,32 +4,18 @@ angular.module('hz').directive({
 
     return {
       restrict: 'A',
-      require: 'ngModel',
+      require: ['selection', 'ngModel' ],
       transclude: true,
       scope: {
         availables: '=elts',
         id: '@'
       },
-      controller: ['$scope', function ($scope) {
-
-        this.toggle = function (elt) {
-
-          var index = $scope.availables.indexOf(elt);
-          if (index !== -1) {
-            $scope.selected.push(elt);
-            $scope.availables.splice(index, 1);
-          } else {
-            index = $scope.selected.indexOf(elt);
-            $scope.selected.splice(index, 1);
-            $scope.availables.push(elt);
-          }
-          $scope.modelCtrl.$setViewValue($scope.selected);
-        };
-      }],
+      controller: function () {},
       template:
           '<div id="{$ id $}" class="selection">\n' +
           '  <label for="selected_{$ id $}">Selected {$ label $}</label>\n' +
-          '  <ul id="selected_{$ id $}" class="selected">' +
+          '  <ul id="selected_{$ id $}" class="selected">\n' +
+          '    <li ng-if="!selected.length"><div class="alert alert-info" >No {$ id $} selected. Please add a {$ id $} from the list below</div></li>\n' +
           '    <li ng-repeat="s in selected"><div data-selection-element data-elt="s"></div></li>\n' +
           '  </ul>\n' +
           '  <label for="available_{$ id $}">Available {$ label $}</label>\n' +
@@ -37,11 +23,26 @@ angular.module('hz').directive({
           '    <li ng-repeat="a in availables"><div data-selection-element data-elt="a"></div></li>\n' +
           '  </ul>\n' +
           '</div>',
-      link: function (scope, element, attrs, modelCtrl, transclude) {
-        scope.modelCtrl = modelCtrl;
-        scope.selected = [];
-        scope.$transcludeFn = transclude;
+      link: function (scope, element, attrs, controllers, transclude) {
+        var selectionCtrl = controllers[0];
+        var modelCtrl = controllers[1];
 
+        scope.selected = [];
+
+        selectionCtrl.toggle = function (elt) {
+          var index = scope.availables.indexOf(elt);
+          if (index !== -1) {
+            scope.selected.push(elt);
+            scope.availables.splice(index, 1);
+          } else {
+            index = scope.selected.indexOf(elt);
+            scope.selected.splice(index, 1);
+            scope.availables.push(elt);
+          }
+          modelCtrl.$setViewValue(scope.selected);
+        }
+
+        scope.$transcludeFn = transclude;
       }
     };
   }],
